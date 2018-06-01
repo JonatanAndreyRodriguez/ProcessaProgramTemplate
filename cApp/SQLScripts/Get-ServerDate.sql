a12a5460a7ee51f083187b7f308fda3d430454d9
R&D Processa <RD-Processa@users.noreply.github.com>
2018-06-01 09:15:20 -0500

    Add files via upload

---
+++ cApp/SQLScripts/Get-ServerDate.sql
@@ -0,0 +1,7 @@
+-- ======================================================================================================================
+-- Author		: <Nombre1 Nombre2 Apellido1 Apellido2>
+-- Create date	: <00 de Mes de 1900>
+-- Description	: <Descripcion del Script>
+-- Gemini		: <<%=$PLASTER_PARAM_ProgramGemini%>>
+-- ======================================================================================================================
+SELECT GETDATE() AS ServerDate
